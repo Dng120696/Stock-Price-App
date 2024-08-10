74a3d92950fba280f91c81ad21b06123ba8ec723
Christian Patrick Nebab <dongyang0216@gmail.com>
2024-08-10 20:13:13 +0800

    added functionality and use the Finnhub API for searching stock price and added some style for the app

--- README.md
+++ README.md
@@ -1,8 +1,55 @@
-# React + Vite
+# Stock Price App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This React-Vite application allows users to search for stock prices by entering a stock symbol. It dynamically displays the current stock price and provides clear error messages for invalid symbols or failed requests. The app includes a loading indicator to show when data is being fetched and features a modern, user-friendly interface for an optimal user experience.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Technologies Used](#technologies-used)
+
+## Features
+
+- **Search**: Search for stock prices by entering a stock symbol.
+- **Error Handling**: Displaying the error messages for invalid symbols.
+- **Loading States**: Show loading indicators while fetching data from the API.
+
+[Stock Price App]()
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js v18.15.0
+- npm 9.8.1
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Dng120696/Stock-Price-App.git
+   cd Stock-Price-App
+   ```
+
+2. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+3. Start the development Server:
+
+   ```bash
+   npm run dev
+   ```
+
+4. Open http://localhost:5173 to view it in the browser.
+
+## Technologies Used
+
+- React
+- Tailwind CSS
+- Finnhub Stock API
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,114 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello World !</h1>;
+  const [stockData, setStockData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [inputVal, setInputVal] = useState("");
+  const [stockQuery, setStockQuery] = useState("");
+  const apiKey = import.meta.env.VITE_FINNHUB_API_KEY;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // guardclause, it will do anything if the input is empty and return nothing
+    if (inputVal.trim().length === 0) return;
+
+    setStockQuery(inputVal.trim()); // Update query
+    setInputVal(""); //clear the input after submit
+    setLoading(true); //it show the loading before the data fetch
+    setStockData(null); // Clear previous stock data
+    setErrorMessage(null); // Clear previous error message
+
+    try {
+      const response = await fetch(
+        `https://finnhub.io/api/v1/quote?symbol=${inputVal}&token=${apiKey}`
+      );
+
+      const data = await response.json();
+
+      //check if the API is invalid key
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      // Check if current price data is invalid
+      if (data.c === 0) {
+        throw new Error("Invalid symbol or no data found"); // create a custom error if the symbol is invalid
+      } else {
+        setStockData(data); // save the data to a state variable
+        setErrorMessage(null); // clear error message
+      }
+    } catch (error) {
+      console.error("Fetch error:", error.message);
+      setStockData(null);
+      setErrorMessage(error.message);
+    } finally {
+      // Always hide loading indicator
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div
+      className="w-[clamp(30rem,90%,120rem)] mx-auto flex items-center justify-center
+    min-h-screen flex-col gap-10 text-2xl"
+    >
+      <h1 className="text-5xl font-bold tracking-[2px] text-gray-700 mb-6">
+        Stock Price App
+      </h1>
+      <div className="w-[50rem] shadow-[0_0_1rem_rgba(0,0,0,0.15)] p-10 rounded-md">
+        <form onSubmit={handleSubmit} className="flex items-center gap-6 ">
+          <input
+            type="text"
+            name="searchStockQuery"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+            placeholder="Search Stock..."
+            className="w-full border border-gray-400 px-6 py-2 rounded outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            type="submit"
+            className="px-6 py-2 border rounded bg-blue-500 text-white hover:bg-blue-600"
+          >
+            Search
+          </button>
+        </form>
+        <div
+          className="h-[20vh] flex items-center flex-col
+         justify-center font-medium text-gray-700"
+        >
+          {loading && <p>Loading...</p>}
+          {!stockQuery && !loading && (
+            <p>Search Stock Symbol to view the Current Price</p>
+          )}
+          {errorMessage && stockQuery && (
+            <p className="text-red-500">Error: {errorMessage}</p>
+          )}
+          {stockData && !errorMessage && stockQuery && (
+            <div
+              className="
+            flex items-center flex-col gap-2"
+            >
+              <p>
+                <span className=" text-gray-500 font-normal">
+                  Stock Symbol:
+                </span>{" "}
+                {stockQuery}
+              </p>
+              <p>
+                {" "}
+                <span className=" text-gray-500 font-normal">
+                  Current Price:{" "}
+                </span>{" "}
+                ${stockData.c}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
